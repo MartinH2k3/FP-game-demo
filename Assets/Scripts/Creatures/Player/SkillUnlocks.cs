5e4ef895011be1fa09be4746fbec0ccf708d2c86
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-06-24 23:53:25 +0200

    feat: Serializable class that holds unlocks for skills

---
+++ Assets/Scripts/Creatures/Player/SkillUnlocks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Creatures.Player
+{
+
+[Serializable]
+public class SkillUnlocks
+{
+    public Dictionary<Skill, bool> Abilities = new();
+
+    public bool IsUnlocked(Skill skill) =>
+        Abilities.TryGetValue(skill, out var unlocked) && unlocked;
+
+    public void Unlock(Skill skill) =>
+        Abilities[skill] = true;
+
+    public void Lock(Skill skill) =>
+        Abilities[skill] = false;
+}
+
+}
