2ed8d19f0d344dbe8c49ee09d05058e4843cdf9a
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-06-28 17:30:25 +0200

    feat: Added class holding base values (can be increased by upgrades)

---
+++ Assets/Scripts/Creatures/Player/BaseStats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Creatures.Player
+{
+[Serializable]
+public class BaseStats
+{
+    public int healthPoints = 100;
+    public int mana = 50;
+    public int strength = 0;
+    public int agility = 0;
+    public int intelligence = 0;
+    public int speed = 0;
+    public int durability = 0;
+    public int fireResistance = 0;
+    public int magicResistance = 0;
+
+}
+}
