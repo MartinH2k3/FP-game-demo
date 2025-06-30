039402460ec3a5caacbbb16989fe3e7e0c84f860
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-06-30 20:55:15 +0200

    feat: Added more base stats to be changed

--- Assets/Scripts/Characters/Player/BaseStats.cs
+++ Assets/Scripts/Characters/Player/BaseStats.cs
@@ -8,6 +8,11 @@ public class BaseStats
 {
     public int healthPoints = 100;
     public int mana = 50;
+    public float attackSpeed = 0.25f;
+    public int attackDamage = 10;
+    public int attackDamageModifier = 1; // constant modifier, multiplied by strength and added to attackDamage
+    public int magicDamage = 5;
+    public int magicDamageModifier = 1; // constant modifier, multiplied by intelligence and added to magicDamage
     public int strength = 0;
     public int agility = 0;
     public int intelligence = 0;
