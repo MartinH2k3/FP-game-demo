6d4594ac85e5f2c5d38ddcf596d97d947a7d3997
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-06-30 20:47:35 +0200

    feat: Helper methods class helping :P

---
+++ Assets/Scripts/Helpers/HelperMethods.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Helpers
+{
+public static class HelperMethods
+{
+    public static bool LayerInLayerMask(int layer, LayerMask layerMask)
+    {
+        return (layerMask & (1 << layer)) != 0;
+    }
+}
+}
