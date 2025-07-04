8f9a44a21d0756dd03306c8b7d1798a7ad9ffce0
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-07-04 23:35:03 +0200

    Refactor: fixed horizontal vs vertical naming mistake

--- Assets/Scripts/Physics/CrowdControl.cs
+++ Assets/Scripts/Physics/CrowdControl.cs
@@ -4,10 +4,10 @@ namespace Physics
 {
 public static class CrowdControl
 {
-    public static void Knockback(Character target, float verticalVelocity, float horizontalVelocity, int direction = 1, float duration = 0.3f) {
+    public static void Knockback(Character target, float horizontalVelocity, float verticalVelocity, int direction = 1, float duration = 0.3f) {
         if (target == null) return;
 
-        target.SetVelocity(direction * verticalVelocity, horizontalVelocity);
+        target.SetVelocity(direction * horizontalVelocity, verticalVelocity);
         target.SetMovementTimeout(duration);
     }
 }
