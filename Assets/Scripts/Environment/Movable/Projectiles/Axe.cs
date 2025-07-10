8f9563312f36af711541bf6fa9398492f89e3212
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-07-10 22:30:38 +0200

    feat: Axe init

--- Assets/Scripts/Environment/Movable/Projectiles/Axe.cs
+++ Assets/Scripts/Environment/Movable/Projectiles/Axe.cs
@@ -1,16 +1,18 @@
+using Physics;
 using UnityEngine;
 
-public class Axe : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+namespace Environment.Movable.Projectiles {
+public class Axe : Projectile, IPhysicsMovable {
+    [SerializeField] private Rigidbody2D rb;
+    public Rigidbody2D Rigidbody => rb;
+
+    public override void Launch() {
+        this.SetVelocity(speed, 0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override void Launch(float x, float y) {
+        var direction = (new Vector2(x, y) - (Vector2)transform.position).normalized;
+        this.SetVelocity(direction * speed);
     }
+}
 }
