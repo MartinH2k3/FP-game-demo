9c14a020971d29fed4897c1b13eb8af47b0aceec
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-07-11 18:02:53 +0200

    feat: prefab to game objects accessible via .Prefab() (for items that have prefabs)

---
+++ Assets/Scripts/Helpers/PrefabDictionary.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Helpers {
+
+public class PrefabDictionary: MonoBehaviour {
+    [SerializeField] private GameObject[] prefabs;
+    private Dictionary<string, GameObject> _prefabDictionary;
+    public static PrefabDictionary Instance { get; private set; }
+    private void OnEnable() {
+        Instance = this;
+        foreach (var prefab in prefabs) {
+            _prefabDictionary[prefab.name] = prefab;
+        }
+    }
+
+    public T GetPrefab<T>(string prefabName) {
+        if (_prefabDictionary.TryGetValue(prefabName, out var prefab)) {
+            return prefab.GetComponent<T>();
+        }
+        Debug.LogWarning($"Prefab with name {prefabName} not found in PrefabDictionary.");
+        return default;
+    }
+
+    public T GetPrefab<T>(T item) where T : Component {
+        return GetPrefab<T>(item.gameObject.name);
+    }
+}
+
+}
---
+++ Assets/Scripts/Helpers/UnityClassExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Helpers
+{
+public static class UnityClassExtensions {
+    public static T Prefab<T>(this T item) where T : Component{
+        return PrefabDictionary.Instance.GetPrefab(item);
+    }
+}
+}
