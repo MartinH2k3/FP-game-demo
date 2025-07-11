ad06fdef5bc5d3c0f0f13a4442d77072082bed97
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-07-11 18:01:50 +0200

    fix: Freeze doesn't stop custom collider methods, just the physics

--- Assets/Scripts/Physics/PhysicsMovableExtensions.cs
+++ Assets/Scripts/Physics/PhysicsMovableExtensions.cs
@@ -5,36 +5,39 @@ namespace Physics
 
 public static class PhysicsMovableExtensions {
     public static void AddVelocity(this IPhysicsMovable movable, Vector2 velocity) {
-        if (movable.Rigidbody != null) {
+        if (movable.Rigidbody is not null) {
             movable.Rigidbody.linearVelocity += velocity;
         }
     }
 
     public static void AddVelocity(this IPhysicsMovable movable, float x, float y) {
-        if (movable.Rigidbody != null) {
+        if (movable.Rigidbody is not null) {
             movable.Rigidbody.linearVelocity += new Vector2(x, y);
         }
     }
 
     public static void SetVelocity(this IPhysicsMovable movable, Vector2 velocity) {
-        if (movable.Rigidbody != null) {
+        if (movable.Rigidbody is not null) {
             movable.Rigidbody.linearVelocity = velocity;
         }
     }
 
     public static void SetVelocity(this IPhysicsMovable movable, float x, float y) {
-        if (movable.Rigidbody != null) {
+        if (movable.Rigidbody is not null) {
             movable.Rigidbody.linearVelocity = new Vector2(x, y);
         }
     }
 
     public static void Freeze(this IPhysicsMovable movable) {
         movable.SetVelocity(Vector2.zero);
-        movable.Rigidbody.simulated = false;
+        movable.Rigidbody.angularVelocity = 0;
+        movable.Rigidbody.bodyType = RigidbodyType2D.Kinematic;
+        movable.Rigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
     }
 
     public static void Unfreeze(this IPhysicsMovable movable) {
-        movable.Rigidbody.simulated = true;
+        movable.Rigidbody.bodyType = RigidbodyType2D.Dynamic;
+        movable.Rigidbody.constraints = RigidbodyConstraints2D.None;
     }
 }
 
